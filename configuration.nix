1fd49823a9c810698188514fd1a5f6287b5c85c5
liam <ubuntu9786@gmail.com>
2021-02-16 14:27:59 -0800

    improved nixos and polybar configs

--- configuration.nix
+++ configuration.nix
@@ -1,4 +1,4 @@
-# Welcome to the Liam NixOS config that honestly is mostly stock with awesomewm jammed in
+# Welcome to the Liam NixOS config that is becoming less stock with awesomewm jammed in
 { config, pkgs, ... }:
 
 {
@@ -10,14 +10,13 @@
   # Use the GRUB 2 boot loader.
   boot.loader.grub.enable = true;
   boot.loader.grub.version = 2;
-  # boot.loader.grub.efiSupport = true;
-  # boot.loader.grub.efiInstallAsRemovable = true;
-  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
-  # Define on which hard drive you want to install Grub.
- boot.loader.grub.device = "/dev/nvme0n1"; # or "nodev" for efi only
-
- networking.hostName = "liam-nixos"; # Define your hostname.
-   #networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  boot.loader.grub.device = "/dev/nvme0n1"; # or "nodev" for efi only
+ #Networking 
+  networking.hostName = "liam-nixos"; # Define your hostname.
+  networking.useDHCP = false;
+  networking.interfaces.wlp2s0.useDHCP = true;
+  networking.networkmanager.enable = true;
+  #networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Set your time zone.
   time.timeZone = "America/Los_Angeles";
@@ -25,9 +24,6 @@
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
   # replicates the default behaviour.
-  networking.useDHCP = false;
-  networking.interfaces.wlp2s0.useDHCP = true;
-  networking.networkmanager.enable = true;
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
@@ -41,7 +37,22 @@
   services.xserver.displayManager.gdm.enable = true;
   # services.xserver.desktopManager.gnome3.enable = true;
   services.xserver.windowManager.awesome.enable = true;
+  services.picom = {
+    enable = true;
+    fade = true; 
+    inactiveOpacity = 0.8;
+    shadow = true;
+    fadeDelta = 1;
+    vSync = true;
+ };
 
+ # Graphics
+  services.xserver.videoDrivers = [ "modsetting" ];
+  services.xserver.useGlamor = true;
+  services.xserver.deviceSection = ''Option "TearFree" "true"'';
+  programs.light.enable = true;
+  hardware.nvidiaOptimus.disable = true;
+ 
  
  #  Enable sound.
   sound.enable = true;
@@ -64,7 +75,6 @@
   # $ nix search wget
     environment.systemPackages = with pkgs; [
     wget vim
-    firefox
     rofi
     autorandr
     pkgs.neofetch
@@ -79,27 +89,24 @@
     dmenu
     brightnessctl
     pkgs.zathura
-    pkgs.picom
+    pkgs.python39
   ];
  nixpkgs.config.allowUnfree = true;
  
-# Fonts to install
+ # Fonts to install
   fonts.fonts = with pkgs; [
 	(nerdfonts.override {fonts = [ "Terminus" ]; })
-];
+  ];
 
-# Enable Flatpak
-    xdg.portal.enable = true; # only needed if you are not doing Gnome
-    services.flatpak.enable = true;
+  # power
+  powerManagement.cpuFreqGovernor = "powersave";
+  powerManagement.enable = true;
+  powerManagement.powertop.enable = true;
+  services.tlp.enable = true;
+  services.thermald.enable = true; 
+    #services.logind.lidSwitch = "suspend";
 
- # power
-powerManagement.enable = true;
-powerManagement.powertop.enable = true;
-services.tlp.enable = true;
-#services.thermald.enable = true; 
-#services.logind.lidSwitch = "suspend";
-
-# Trackpad settings
+ # Trackpad settings
   services.xserver.libinput = {
     enable = true;
     naturalScrolling = true;
@@ -108,21 +115,7 @@ services.tlp.enable = true;
     middleEmulation = true;
     accelSpeed = "0.5";
   };
-
- # Graphics
- # services.xserver.videoDrivers = [ "nvidia" ];
-  services.xserver.deviceSection = ''Option "TearFree" "true"'';
-  programs.light.enable = true;
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
+   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
