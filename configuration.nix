97c6f41c6300a3a8f44f88340c3b0ee9085e088f
liam <ubuntu9786@gmail.com>
2021-02-24 18:21:38 -0800

    doas persistance and management of configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -7,10 +7,7 @@
       ./hardware-configuration.nix
     ];
 
-
-  # Use the GRUB 2 boot loader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.version = 2;
+  # replace this with your hdd mount point
   boot.loader.grub.device = "/dev/nvme0n1"; # or "nodev" for efi only
   
   # Networking 
@@ -23,12 +20,66 @@
 ############### REPLACE FROM HERE UP WITH INFORMATION SPECIFIC TO EACH BUILD ###################
 
 
+  # Use the GRUB 2 boot loader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+
   # Set your time zone.
   time.timeZone = "America/Los_Angeles";
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
+ 
+  # power
+  powerManagement.cpuFreqGovernor = "powersave";
+  powerManagement.enable = true;
+  powerManagement.powertop.enable = true;
+  services.tlp.enable = true;
+  services.thermald.enable = true; 
   
+ # Splash screen
+  boot.plymouth.enable = true;
+  boot.plymouth.theme = "breeze";
+
+
+ # Trackpad settings
+  services.xserver.libinput= {
+    enable = true;
+    touchpad.naturalScrolling = true;
+    touchpad.tapping = true;
+    touchpad.tappingDragLock = false;
+    touchpad.middleEmulation = true;
+    touchpad.accelSpeed = "0.5";
+  };
+
+
+  # Graphics
+  services.xserver.videoDrivers = [ "modsetting" ];
+  services.xserver.useGlamor = true;
+  services.xserver.deviceSection = ''Option "TearFree" "true"'';
+  programs.light.enable = true;
+  hardware.nvidiaOptimus.disable = true;
+ 
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+  sound.mediaKeys.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.liam = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" ]; # Enable ‘sudo’ for the user.
+  };
+
+  # Enable doas instead of sudo
+    security.sudo.enable = false;
+    security.doas.enable = true;
+    security.doas.extraRules = [{
+	users = [ "liam" ];
+	keepEnv = true;
+        persist = true;
+     }];
+
   # Enable AwesomeWM
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
@@ -42,33 +93,13 @@
     vSync = true;
     };
 
-  # Graphics
-  services.xserver.videoDrivers = [ "modsetting" ];
-  services.xserver.useGlamor = true;
-  services.xserver.deviceSection = ''Option "TearFree" "true"'';
-  programs.light.enable = true;
-  hardware.nvidiaOptimus.disable = true;
- 
- 
-  # Enable sound.
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
-
-  # Set Fish as shell
+ # Set Fish as shell
    programs.fish.enable = true;
    users.users.liam = {
    shell = pkgs.fish;
   };
 
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.liam = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" "networkmanager" ]; # Enable ‘sudo’ for the user.
-  };
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
+  # List packages installed in system profile.
     environment.systemPackages = with pkgs; [
     wget
     vim
@@ -99,55 +130,16 @@
     scrot
     pfetch
   ];
- nixpkgs.config.allowUnfree = true;
+     nixpkgs.config.allowUnfree = true;
  
  # Fonts to install
   fonts.fonts = with pkgs; [
 	(nerdfonts.override {fonts = [ "Terminus" ]; })
-  ];
+      ];
+ # Use most up to date Linux Kernals 
+   # boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  # power
-  powerManagement.cpuFreqGovernor = "powersave";
-  powerManagement.enable = true;
-  powerManagement.powertop.enable = true;
-  services.tlp.enable = true;
-  services.thermald.enable = true; 
-
-  # Enable doas instead of sudo
-    security.doas.enable = true;
-    security.sudo.enable = false;
-  # Configure doas
-    security.doas.extraRules = [{
-	users = [ "liam" ];
-	keepEnv = true;
-     }];
-
-
- # Change the kernal version
-  # boot.kernelPackages = pkgs.linuxPackages_latest;
-  
- # Splash screen
-  boot.plymouth.enable = true;
-  boot.plymouth.theme = "breeze";
-
-
- # Trackpad settings
-  services.xserver.libinput= {
-    enable = true;
-    touchpad.naturalScrolling = true;
-    touchpad.tapping = true;
-    touchpad.tappingDragLock = false;
-    touchpad.middleEmulation = true;
-    touchpad.accelSpeed = "0.5";
-  };
-
-
- # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+# NixOS Configuration 
   system.stateVersion = "20.09"; # Did you read the comment?
   system.autoUpgrade = {
        enable = true;
